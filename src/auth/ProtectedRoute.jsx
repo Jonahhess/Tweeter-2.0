f021b04ae86a821477ccc22ba97b3986e773512b
jonahhess <jonahhessdev@gmail.com>
2025-08-17 23:23:31 +0300

    login mandatory

--- src/auth/ProtectedRoute.jsx
+++ src/auth/ProtectedRoute.jsx
@@ -4,7 +4,7 @@ import { useAuth } from "./AuthProvider";
 function ProtectedRoute({ children }) {
   const { activeUser } = useAuth();
 
-  if (!activeUser) return <Navigate to="/" replace />;
+  if (!activeUser) return <Navigate to="/login" replace />;
 
   return <>{children}</>;
 }
