dbf891c289aaf75df0925260423a943cabd0c3f3
jonahhess <jonahhessdev@gmail.com>
2025-08-17 23:30:59 +0300

    removed logo section

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,13 +2,8 @@ import { Link } from "react-router";
 const Navigation = () => {
   return (
     <div>
-      <Link to="/">
-        <h1>My App</h1>
-      </Link>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/profile">Profile</Link>
-      </div>
+      <Link to="/">Home</Link>
+      <Link to="/profile">Profile</Link>
     </div>
   );
 };
