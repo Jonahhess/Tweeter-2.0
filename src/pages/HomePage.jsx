6394e25dc7bb1e8dfa9249abdc6612a57f8c5cca
jonahhess <jonahhessdev@gmail.com>
2025-08-17 23:18:57 +0300

    conditional rendering done simple

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router";
+import { useAuth } from "../auth/AuthProvider";
 
 export default function HomePage() {
+  const { activeUser, handleLogout } = useAuth();
   const navigate = useNavigate();
   return (
     <>
-      Hello world
-      <button onClick={() => navigate("/login")}>Login</button>
+      {!activeUser && <button onClick={() => navigate("/login")}>Login</button>}
+      {activeUser && <button onClick={handleLogout}>Log Out</button>}
     </>
   );
 }
