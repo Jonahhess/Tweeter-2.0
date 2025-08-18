532c0583cb5353c6d1e1a44190b0005aa084d817
jonahhess <jonahhessdev@gmail.com>
2025-08-18 12:08:55 +0300

    profile now contains # posts

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -1,6 +1,12 @@
 import { useAuth } from "../auth/AuthProvider";
 
-export default function ProfilePage() {
+export default function ProfilePage({ tweets }) {
   const { activeUser } = useAuth();
-  return <>{activeUser}</>;
+  const myTweets = tweets?.filter((tweet) => tweet.username === activeUser);
+  return (
+    <>
+      {activeUser} has: {myTweets.length || 0} tweet
+      {myTweets.length !== 1 ? "s" : ""}
+    </>
+  );
 }
