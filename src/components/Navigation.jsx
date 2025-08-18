4c53dd6bfcdb47c3d4d6238273d94ff1c589a87e
jonahhess <jonahhessdev@gmail.com>
2025-08-18 12:07:49 +0300

    navbar more welcoming

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -8,8 +8,12 @@ const Navigation = () => {
     <div>
       <Link to="/">Home</Link>
       <Link to="/profile">Profile</Link>
-      {!activeUser && <button onClick={() => navigate("/login")}>Login</button>}
-      {activeUser && <button onClick={handleLogout}>Log Out</button>}
+      {activeUser && (
+        <>
+          <p>Welcome {activeUser}</p>
+          <button onClick={handleLogout}>Log Out</button>
+        </>
+      )}
     </div>
   );
 };
