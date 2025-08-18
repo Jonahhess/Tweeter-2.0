0557f70a9f041d34cee98e19a6812bf3584bf8b5
jonahhess <jonahhessdev@gmail.com>
2025-08-18 12:07:29 +0300

    made component more readable

--- src/components/ReadOnlyTweet.jsx
+++ src/components/ReadOnlyTweet.jsx
@@ -1,11 +1,11 @@
-import { Card, Button, Group } from "@mantine/core";
+import { Card, Text } from "@mantine/core";
 
 export default function ReadOnlyTweet({ content, username, date }) {
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Text size="md">{username}</Text>
-      <Text size="sm">{content}</Text>
-      <Text>{date}</Text>
+      <Text fw={500}>username: {username}</Text>
+      <Text size="sm">content: {content}</Text>
+      <p>date: {date}</p>
     </Card>
   );
 }
