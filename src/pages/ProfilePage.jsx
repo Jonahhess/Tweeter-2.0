fd6933db728d10ee1b07f366f2d34854c622967c
jonahhess <jonahhessdev@gmail.com>
2025-08-18 13:38:03 +0300

    styling

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -4,9 +4,9 @@ export default function ProfilePage({ tweets }) {
   const { activeUser } = useAuth();
   const myTweets = tweets?.filter((tweet) => tweet.username === activeUser);
   return (
-    <>
+    <div style={{ textAlign: "center", marginTop: "0.1rem" }}>
       {activeUser} has: {myTweets.length || 0} tweet
       {myTweets.length !== 1 ? "s" : ""}
-    </>
+    </div>
   );
 }
