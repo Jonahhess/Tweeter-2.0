ac3d21c44d876419c44abcf1e5deab0e2127f194
jonahhess <jonahhessdev@gmail.com>
2025-08-18 12:08:11 +0300

    made component more readable

--- src/components/Tweet.jsx
+++ src/components/Tweet.jsx
@@ -1,18 +1,16 @@
-import { Card, Button, Group } from "@mantine/core";
+import { Card, Button } from "@mantine/core";
 
-export default function Tweet({ title, newTweet, setNewTweet }) {
+export default function Tweet({ title, newTweet, setNewTweet, addTweet }) {
   return (
     <Card id="card" shadow="sm" padding="lg" radius="md" withBorder>
       <h1>{title}</h1>
-      <Group justify="space-between" mt="md" mb="xs">
-        <input
-          type="text"
-          placeholder="tweet"
-          value={newTweet}
-          onChange={(e) => setNewTweet(e.target.value)}
-        />
-      </Group>
-      <Button onClick={() => addTweet(content)}>Submit</Button>
+      <textarea
+        type="text"
+        placeholder="tweet"
+        value={newTweet}
+        onChange={(e) => setNewTweet(e.target.value)}
+      />
+      <Button onClick={addTweet}>Submit</Button>
     </Card>
   );
 }
