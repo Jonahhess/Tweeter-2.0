c5849957f6b4026fb21ff3cbf17be040a7ca9ef5
jonahhess <jonahhessdev@gmail.com>
2025-08-18 12:21:43 +0300

    added styling to navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,19 +1,31 @@
-import { Link, useNavigate } from "react-router";
+import { Link } from "react-router";
 import { useAuth } from "../auth/AuthProvider";
 
 const Navigation = () => {
-  const navigate = useNavigate();
   const { activeUser, handleLogout } = useAuth();
   return (
-    <div>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        gap: "1rem",
+        minHeight: "60px",
+        padding: "0 1rem",
+        boxSizing: "border-box",
+        backgroundColor: "lightgrey",
+      }}
+    >
       <Link to="/">Home</Link>
       <Link to="/profile">Profile</Link>
-      {activeUser && (
-        <>
-          <p>Welcome {activeUser}</p>
-          <button onClick={handleLogout}>Log Out</button>
-        </>
-      )}
+      <div style={{ display: "flex", gap: "1rem" }}>
+        {activeUser && (
+          <>
+            <p>Welcome {activeUser}</p>
+            <button onClick={handleLogout}>Log Out</button>
+          </>
+        )}
+      </div>
     </div>
   );
 };
