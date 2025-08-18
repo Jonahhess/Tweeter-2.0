222d0b4916a3c22e652e26d18e42e0578ef55cfe
jonahhess <jonahhessdev@gmail.com>
2025-08-18 10:01:34 +0300

    adding logout to navbar

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,15 @@
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useAuth } from "../auth/AuthProvider";
+
 const Navigation = () => {
+  const navigate = useNavigate();
+  const { activeUser, handleLogout } = useAuth();
   return (
     <div>
       <Link to="/">Home</Link>
       <Link to="/profile">Profile</Link>
+      {!activeUser && <button onClick={() => navigate("/login")}>Login</button>}
+      {activeUser && <button onClick={handleLogout}>Log Out</button>}
     </div>
   );
 };
