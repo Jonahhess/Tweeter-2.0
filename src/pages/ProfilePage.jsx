9fb49801e8149027333c1dbb79a42ad9bc9159dc
jonahhess <jonahhessdev@gmail.com>
2025-08-19 23:54:23 +0300

    activeUser now obj

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -4,10 +4,10 @@ import { useTweets } from "../components/TweetsProvider";
 export default function ProfilePage() {
   const { activeUser } = useAuth();
   const { tweets } = useTweets();
-  const myTweets = tweets?.filter((tweet) => tweet.userName === activeUser);
+  const myTweets = tweets?.filter((tweet) => tweet.email === activeUser.email);
   return (
     <div style={{ textAlign: "center", marginTop: "0.1rem" }}>
-      {activeUser} has: {myTweets?.length || 0} tweet
+      {activeUser.email} has: {myTweets?.length || 0} tweet
       {myTweets?.length !== 1 ? "s" : ""}
     </div>
   );
