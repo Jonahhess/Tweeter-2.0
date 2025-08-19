a022538739b42066607b4d4efd075d9391c0aa0b
jonahhess <jonahhessdev@gmail.com>
2025-08-19 13:44:00 +0300

    change var name

--- src/pages/ProfilePage.jsx
+++ src/pages/ProfilePage.jsx
@@ -2,7 +2,7 @@ import { useAuth } from "../auth/AuthProvider";
 
 export default function ProfilePage({ tweets }) {
   const { activeUser } = useAuth();
-  const myTweets = tweets?.filter((tweet) => tweet.username === activeUser);
+  const myTweets = tweets?.filter((tweet) => tweet.userName === activeUser);
   return (
     <div style={{ textAlign: "center", marginTop: "0.1rem" }}>
       {activeUser} has: {myTweets.length || 0} tweet
