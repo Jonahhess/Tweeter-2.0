0667d7d2ee931b7dab905baff2a6f7898fe0ca9d
jonahhess <jonahhessdev@gmail.com>
2025-08-19 13:43:08 +0300

    changed var name

--- src/components/ReadOnlyTweet.jsx
+++ src/components/ReadOnlyTweet.jsx
@@ -1,9 +1,9 @@
 import { Card, Text } from "@mantine/core";
 
-export default function ReadOnlyTweet({ content, username, date }) {
+export default function ReadOnlyTweet({ content, userName, date }) {
   return (
     <Card shadow="sm" padding="lg" radius="md" withBorder>
-      <Text fw={500}>username: {username}</Text>
+      <Text fw={500}>userName: {userName}</Text>
       <Text size="sm">content: {content}</Text>
       <p>date: {date}</p>
     </Card>
