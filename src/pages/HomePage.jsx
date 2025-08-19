73b7330cc74d885e4ddbfad07179d87ee227a312
jonahhess <jonahhessdev@gmail.com>
2025-08-19 17:09:03 +0300

    no return value needed

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -20,7 +20,7 @@ export default function HomePage() {
       };
       setNewTweet("");
       try {
-        const post = await handlePost(tweet);
+        await handlePost(tweet);
       } catch (e) {
         console.log(e);
       } finally {
