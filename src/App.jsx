bceb5cc3d73f258624747c07dbcfbab018e51d43
jonahhess <jonahhessdev@gmail.com>
2025-08-19 16:51:35 +0300

    moved tweets logic to context

--- src/App.jsx
+++ src/App.jsx
@@ -4,51 +4,37 @@ import HomePage from "./pages/HomePage";
 import LoginPage from "./pages/LoginPage";
 import ProtectedRoute from "./auth/ProtectedRoute";
 import { AuthProvider } from "./auth/AuthProvider";
+import { TweetsProvider } from "./components/TweetsProvider";
 import ProfilePage from "./pages/ProfilePage";
 import Layout from "./components/Layout";
-import { useEffect, useState } from "react";
-import { getTweets, postTweet } from "./data/serverAPI";
 
 function App() {
-  const [tweets, setTweets] = useState(null);
-
-  useEffect(() => {
-    async function getData() {
-      const data = await getTweets();
-      setTweets(data);
-    }
-    getData();
-  }, []);
-
-  async function handlePost(tweet) {
-    const post = await postTweet(tweet);
-    return post;
-  }
-
   return (
     <>
       <AuthProvider>
         <Layout>
-          <Routes>
-            <Route
-              path="/"
-              element={
-                <ProtectedRoute>
-                  <HomePage tweets={tweets} handlePost={handlePost} />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="/login" element={<LoginPage />} />
-            <Route
-              path="/profile"
-              element={
-                <ProtectedRoute>
-                  <ProfilePage tweets={tweets} />
-                </ProtectedRoute>
-              }
-            />
-            <Route path="*" element={<Navigate to="/" replace />} />
-          </Routes>
+          <TweetsProvider>
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <ProtectedRoute>
+                    <HomePage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/login" element={<LoginPage />} />
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <ProfilePage />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="*" element={<Navigate to="/" replace />} />
+            </Routes>
+          </TweetsProvider>
         </Layout>
       </AuthProvider>
     </>
