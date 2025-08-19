32210c1c6b3a5905fbe656a1bb62c9c3c0144271
jonahhess <jonahhessdev@gmail.com>
2025-08-19 23:54:12 +0300

    added password field

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,4 +1,4 @@
-import { Button, Group, TextInput } from "@mantine/core";
+import { Button, TextInput } from "@mantine/core";
 import { useForm } from "@mantine/form";
 import { useAuth } from "../auth/AuthProvider";
 
@@ -9,25 +9,37 @@ export default function LoginPage() {
     mode: "uncontrolled",
     initialValues: {
       activeUser: "",
+      password: "",
     },
 
     validate: {
       activeUser: (value) =>
         /^[a-zA-Z].{1,}$/.test(value) ? null : "Invalid userName",
     },
+    password: (value) =>
+      /^[a-zA-Z].{1,}$/.test(value) ? null : "Invalid password",
   });
 
   return (
     <form
       style={{ display: "flex", marginTop: "0.25rem" }}
-      onSubmit={form.onSubmit((values) => handleLogin(values.activeUser))}
+      onSubmit={form.onSubmit((values) =>
+        handleLogin(values.activeUser, values.password)
+      )}
     >
       <TextInput
         w={200}
         withAsterisk
-        placeholder="Enter userName"
+        placeholder="Enter email"
         key={form.key("activeUser")}
         {...form.getInputProps("activeUser")}
+      />
+      <TextInput
+        w={200}
+        withAsterisk
+        placeholder="Enter password"
+        key={form.key("password")}
+        {...form.getInputProps("password")}
       />
       <Button
         style={{ alignSelf: "flex-start", marginLeft: "0.1rem" }}
